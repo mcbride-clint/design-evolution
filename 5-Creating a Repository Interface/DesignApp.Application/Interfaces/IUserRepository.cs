114598262c09ede95f3e869459dbb88743902dc6
mcbride-clint <cmm5559@gmail.com>
2020-04-30 10:54:32 -0400

    Complete Step 5 Creating Repository Interface

---
+++ 5-Creating a Repository Interface/DesignApp.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using DesignApp.Domain.Models;
+using System.Collections.Generic;
+
+namespace DesignApp.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        User Find(string userId);
+        List<User> GetAll();
+        User Update(User user);
+    }
+}
